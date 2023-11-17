a11bffc3eee32fad1949954b86f7b1c4acf3077f
marksikaundi <emmanuelmark117@gmail.com>
2023-11-17 11:55:49 +0200

    pagination completed

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`)
     const params = new URLSearchParams(searchParams)
+    params.set('page', '1');
     if (term){
       params.set('query', term);
     } else{
